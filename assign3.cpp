d91a73283dc7c98edffefbd8f9a962807fc3750e
LemuelTembi <43863288+LemuelTembi@users.noreply.github.com>
2022-10-02 21:02:52 -0500

    Updated assign3.cpp
    assign3.cpp now compiles and creates an executable without any errors but there are still some problems. choosing option 1 and inputing 'r' as the book type will cause the program to get stuck in an infinite loop. And you also have to work on the restrictors because if I try jumping to page 2000 it says "you jumped forward by 2000" but I stopped jumping over the max or below the min number of pages in book.cpp so it does not match. You should do the same thing for jumping backward. It should tell them that you cannot jump that far forward or backward or something.

--- assign3.cpp
+++ assign3.cpp
@@ -10,8 +10,8 @@
 
 //  ======================= symbolic constrantes =================
 const int MIN = 1;
-const int MAX = 8;
-const int DECIMAL = 2; 
+const int MAX = 7;
+const int DECIMAL = 2;
 
 //  ======================= namespaces used ======================
 using namespace std;
@@ -19,11 +19,11 @@ using namespace std;
 // ======================== function prototypes ==================
 
 void displayMenu();
-void getChoice(int &);
-void takeAction(int, Book &);
+void getChoice(int&);
+void takeAction(int, Book&);
 
 
-int main(){//beginning of int main 
+int main(){//beginning of int main
 
     Book book1("915934560213", "The lord of the rings", "J. R. R. Tolkien", 1178);
 
@@ -33,27 +33,28 @@ int main(){//beginning of int main
    {
       displayMenu();
       getChoice(choice);
-      takeAction(choice, testCar);
+      takeAction(choice, book1);
    } while (choice != MAX);
 
    return 0;
 
-}// end of main 
+}// end of main
 
 
 void displayMenu()
 // this funciton displays the main menu of the program
 {
+    cout << "*********************************" << endl;
     cout << "Test book driver" << endl;
-    cout << "1.  Acquire Book button" << endl; 
+    cout << "1.  Acquire Book button" << endl;
     cout << "2.  Press Read button" << endl;
     cout << "3.  Press Jump button" << endl;
     cout << "4.  Press Backup button" << endl;
     cout << "5.  Press Return Book button" << endl;
-    cout << "7.  Display book state" << endl;
-    cout << "8.  Exit" << endl;
-    cout << "please enter your choice:   " << endl;
-}// end of the main menu 
+    cout << "6.  Display book state" << endl;
+    cout << "7.  Exit" << endl;
+    cout << "please enter your choice: ";
+}// end of the main menu
 
 void getChoice(int &option)
 //this function asks the user to input a choice based on the menu options.
@@ -61,7 +62,7 @@ void getChoice(int &option)
 //to reenter the value. this will continue until the user inputs a value that
 //is int he bounds.
 {
-     do
+   do
    {
       cin >> option;
       if (option < MIN || option > MAX)
@@ -71,64 +72,72 @@ void getChoice(int &option)
               << " is out of bounds of the given range a values ."
               << endl;
          cout << "Please enter the value agine ";
-      }  
+      }
    } while (option < MIN || option > MAX);
-}// end of function getChoice 
+}// end of function getChoice
 
 void takeAction(int choice, Book &book1)
 {
-
+    int newBookStatus;
+    char newBookType;
+    int addPages;
+    int subPages;
+    float amtPaid;
 
     switch(choice)
     //switch statment to do the required action based on the
     //choice of the user.
     {
-        char newBookStatus; 
-        int newBookType;
-        int addPages;
-        int subPages; 
-        float amtPaid;
-
-        case 1: cout << "option 1 was selected" << endl;
-                cout << "please enter book status(R/r for recreational, A/a for academic, N/n-neither): " << endl;
+        case 1: cout << endl << "Option 1 was selected... " << endl;
+                cout << "Please enter book status(R/r for recreational, A/a for academic, N/n-neither): ";
                 cin >> newBookStatus;
-                cout << "please enter book type(1- purchased, 2-rented, 3-neither of these):" << endl; 
+                cout << "Please enter book type(1- purchased, 2-rented, 3-neither of these): ";
                 cin >> newBookType;
-                cout << "please enter amount paid for book: " << endl; 
-                cin >> amtPaid; 
-                book1.acquireBook(char newBookStatus, int newBookStatus, float amtPaid);
+                cout << "Please enter amount paid for book: ";
+                cin >> amtPaid;
+                book1.acquireBook(newBookType, newBookStatus, amtPaid);
+                cout << endl;
                 break;
-        
-        case 2: cout << "option 2 was selected" <<  endl;
+
+        case 2: cout << "option 2 was selected... " << endl;
+                cout << "Page read!" << endl;
                 book1.readPage();
+                cout << endl;
                 break;
-        
-        case 3: cout << "option 3 was selected" << endl;
-                cout << "please enter the number of pages you want to go forward: " << endl;
+
+        case 3: cout << "option 3 was selected... " << endl;
+                cout << "please enter the number of pages you want to go forward: ";
                 cin >> addPages;
-                book1.jumpPages(int addPages);
+                book1.jumpPages(addPages);
+                cout << "You went forward by " << addPages << " number of pages!" << endl;
+                cout << endl;
                 break;
-        
-        case 4: cout << "option 4 was selected" << endl;
-                cout << "please enter the number of pages you want to go backwards: " << endl;
-                cin >> subPages; 
-                book1.backup(int subPages);
+
+        case 4: cout << endl << "option 4 was selected... " << endl;
+                cout << "please enter the number of pages you want to go backwards: ";
+                cin >> subPages;
+                book1.backup(subPages);
+                cout << "You went backwards by " << subPages << " number of pages!" << endl;
+                cout << endl;
                 break;
-        
-        case 5: cout << "option 5 was selected " << endl;
+
+        case 5: cout << endl << "option 5 was selected... " << endl;
                 book1.returnBook();
-                cout << "you have returned to book. " << endl;
+                cout << "you have returned to book. ";
+                cout << endl;
                 break;
 
         case 6: cout << "option 6 was selected "<< endl;
-                cout << "the state of the book will now be displayed:" << endl;
+                cout << "the state of the book will now be displayed:" << endl << endl;
+                cout << "**************************************************" << endl;
                 cout << "The Book title is: " << book1.getISBN() << endl;
                 cout << "The Book type is: " << book1.getBookType() << endl;
                 cout << "The Book Status is: " << book1.getBookStatus() << endl;
                 cout << "The number of pages of the book is: " << book1.getPages() << endl;
                 cout << "The current page of the book is: " << book1.getCurrentPage() << endl;
+                cout << endl;
                 break;
-        
+
         default:
                 break;
 
@@ -137,3 +146,9 @@ void takeAction(int choice, Book &book1)
 
     }
 }
+
+/*void createNewBook(string varName, string ISBN, string bookTitle, string author, int numPages)
+{
+   Book varName(ISBN, bookTitle, author, numPages);
+}
+*/
