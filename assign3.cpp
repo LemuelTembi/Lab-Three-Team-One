ad2a8e5a9b9f1487bcc81f9d890864b340cff6b9
LemuelTembi <43863288+LemuelTembi@users.noreply.github.com>
2022-10-12 16:14:14 -0500

    Update assign3.cpp

--- assign3.cpp
+++ assign3.cpp
@@ -3,234 +3,150 @@
 //  Book class.
 
 //  ========================= header files =======================
-#include <iostream>
-#include <string>
-#include <cstring>
-#include "book.h"
-
-//  ======================= symbolic constrantes =================
-const int MIN = 1;
-const int MAX = 7;
-const int DECIMAL = 2;
-const int librarySize = 20;
+#include <iostream>                       // for console I/O
+#include <string>                         // for string library
+#include <cstring>                       // for c string library
+#include "book.h"                   // for Employee class
 
 //  ======================= namespaces used ======================
-using namespace std;
+using namespace std;                    // for std functions
 
-// ======================== function prototypes ==================
 
-void displayMenu();
-void getChoice(int&);
-void takeAction(int, Book&);
-void addNewKnownBook(string, string, string);
-void newUnknownBook();
-
-//int library[librarySize];
-//int &bookIndex;
-
-Book book1("3972640326748","Lord of the rings", "Jrr Token", 200);
-
-int main(){//beginning of int main
-
-    /*for (int i = 0; i < librarySize; i++)
-    {
-         cout << library[i] << endl << endl;
-    }*/
-
-    int choice;
-
-    do
-   {
-      displayMenu();
-      getChoice(choice);
-      takeAction(choice, book1);
-   } while (choice != MAX);
-
-   return 0;
-
-}// end of main
-
-/*void addNewKnownBook(string ISBN, string bookTitle, string author, int numPages)
+Book::Book(string newISBN, string newTitle,
+            string newAuthor, int newNumPages)
+// purpose: intialize the data members with values
+// precondition: none
+// postcondition: the new variables should be able to change the content
+// of the private variables.
 {
-   bookIndex = 0;
-   Book bookTitle(ISBN, bookTitle, author, numPages);
+   ISBN = newISBN;
+   title = newTitle;
+   author = newAuthor;
+   numPages = newNumPages;
+
+   bookType = 'n';
+   bookStatus = 3;
+   currentPage = 0;
+   price = 0;
+
+} // end of value constructor
+
+// accessor functions to retrieve content of member variables (private)
+string Book::getISBN()
+{
+   return ISBN;
 }
-void addNewUnknownBook (string bookName)
+
+string Book::getTitle()
 {
-   Book bookTitle();
+   return title;
 }
-*/
-void displayMenu()
-// this funciton displays the main menu of the program
-{
-    cout << "*********************************" << endl;
-    cout << "Test book driver" << endl;
-    cout << "1.  Acquire Book button" << endl;
-    cout << "2.  Press Read button" << endl;
-    cout << "3.  Press Jump button" << endl;
-    cout << "4.  Press Backup button" << endl;
-    cout << "5.  Press Return Book button" << endl;
-    cout << "6.  Display book state" << endl;
-    cout << "7.  Exit" << endl;
-    cout << "please enter your choice: ";
-}// end of the main menu
 
-void getChoice(int &option)
-//this function asks the user to input a choice based on the menu options.
-//if the user inputs a value that is out of bounds, the function will ask them
-//to reenter the value. this will continue until the user inputs a value that
-//is int he bounds.
+string Book::getAuthor()
 {
-   do
-   {
-      cin >> option;
-      if (option < MIN || option > MAX)
-      {
-         cout << "The choice: "
-              << option
-              << " is out of bounds of the given range a values ."
-              << endl;
-         cout << "Please enter the value agine ";
-      }
-   } while (option < MIN || option > MAX);
-}// end of function getChoice
+   return author;
+}
 
-void takeAction(int choice, Book &book1)
+float Book::getPrice()
 {
-    int newBookStatus;
-    char newBookType;
-    int addPages;
-    int subPages;
-    float amtPaid;
+   return price;
+}
 
-    switch(choice)
-    //switch statment to do the required action based on the
-    //choice of the user.
+char Book::getBookType()
+{
+   return bookType;
+}
+
+int Book::getBookStatus()
+{
+   return bookStatus;
+}
+
+int Book::getPages()
+{
+   return numPages;
+}
+
+int Book::getCurrentPage()
+{
+   return currentPage;
+}
+
+void Book::acquireBook(int newBookStatus , char newBookType, float amtPaid)
+{
+    if (tolower(newBookType) == 'r' || tolower(newBookType) == 'a')
     {
-        case 1: cout << endl << "Option 1 was selected... " << endl;
-                cout << "Please enter book Type(R/r for recreational, A/a for academic, N/n-neither): ";
-                cin >> newBookType;
-                cin.ignore(200, '\n');
-                cout << "Please enter book status(1- purchased, 2-rented, 3-neither of these): ";
-                cin >> newBookStatus;
-                cin.ignore(200, '\n');
-                cout << "Please enter amount paid for book: ";
-                cin >> amtPaid;
-                cin.ignore(200, '\n');
-                book1.acquireBook(newBookStatus, newBookType, amtPaid);
+        bookType = newBookType;
+    }
+    else
+    {
+        bookType = bookType;
+    }
 
-                cout << endl;
-                break;
+    if (newBookStatus == 1 || newBookStatus == 2)
+    {
+        bookStatus = newBookStatus;
+    }
+    else
+    {
+        bookStatus = bookStatus;
+    }
 
-        case 2: cout << "option 2 was selected... " << endl;
-                if (book1.getBookStatus() != 3)
-                {
-                   book1.readPage();
-                   cout << "Page read!" << endl;
-                }
-                else
-                {
-                   cout << endl << "You cannot read this book. You have neither purchased it nor rented it" << endl;
-                }
-                cout << endl;
-                break;
-
-        case 3: cout << "option 3 was selected... " << endl;
-                if (book1.getBookStatus() != 3)
-                {
-                   cout << "please enter the number of pages you want to go forward: ";
-                   cin >> addPages;
-                   if (addPages > book1.getPages() || addPages > book1.getPages() - book1.getCurrentPage())
-                   {
-                      cout << endl << "You cannot jump by " << addPages << " pages. It is more than the number of pages the book has..." << endl;
-                   }
-                   else if (addPages < 0)
-                   {
-                      cout << endl << "You cannot jump page by a negative number..." << endl;
-                   }
-                   else
-                   {
-                      book1.jumpPages(addPages);
-                      cout << endl << "You went forward by " << addPages << " number of pages!" << endl;
-                   }
-                   cout << endl;
-                }
-                else
-                {
-                   cout << endl << "You cannot read this book. You have neither purchased it nor rented it" << endl;
-                }
-                break;
-
-        case 4: cout << endl << "option 4 was selected... " << endl;
-                if (book1.getBookStatus() != 3)
-                {
-                   cout << "please enter the number of pages you want to go backwards: ";
-                   cin >> subPages;
-                   if (subPages > book1.getCurrentPage())
-                   {
-                      cout << endl << "You cannot backup more than " << book1.getCurrentPage() << " number of pages" << endl;
-                   }
-                   else if (addPages < 0)
-                   {
-                      cout << endl << "You cannot jump page by a negative number..." << endl;
-                   }
-                   else
-                   {
-                      book1.backup(subPages);
-                      cout << "You went backwards by " << subPages << " number of pages!" << endl;
-                   }
-                   cout << endl;
-                }
-                else
-                {
-                   cout << endl << "You cannot read this book. You have neither purchased it nor rented it" << endl;
-                }
-                break;
-
-        case 5: cout << endl << "option 5 was selected... " << endl;
-                if (book1.getBookStatus() == 2)
-                {
-                   book1.returnBook();
-                   cout << "you have returned to book. ";
-                }
-                else
-                {
-                   cout << endl << "You can only return a book you rented!" << endl;
-                }
-                cout << endl;
-                break;
-
-        case 6: cout << "option 6 was selected "<< endl;
-                cout << "the state of the book will now be displayed:" << endl << endl;
-                cout << "**************************************************" << endl;
-                cout << "The Book title is: " << book1.getTitle() << endl;
-                
-                //cout << "The Book type is: " << book1.getBookType() << endl;
-                if (book1.getBookType.tolower() == 'r'){
-                  cout << "The Book type is: recreational" << endl;
-                } else if (book1.getBookType.tolower() == 'a'){
-                  cout << "The Book type is: academic" << endl;
-                } else {
-                  cout << "The Book type is: Neither" << endl;
-                }
-
-                //cout << "The Book Status is: " << book1.getBookStatus() << endl;
-                if (book1.getBookStatus == 1){
-                  cout << "the book Status is: purchased" << endl;
-                } else if (book1.getBookStatus == 2){
-                  cout << "the book status is: reted" << endl;
-                } else {
-                  cout << "the book status is: neither of these" << endl;
-                }
-
-                cout << "The number of pages of the book is: " << book1.getPages() << endl;
-                cout << "The current page of the book is: " << book1.getCurrentPage() << endl;
-                cout << endl;
-                break;
-
-        default:
-                break;
-
+    if (tolower(newBookType) == 'r' || tolower(newBookType) == 'a')
+    {
+        price = amtPaid;
+    }
+    else
+    {
+        price = price;
     }
 }
 
+void Book::readPage()
+{
+   if ((bookStatus != 3) && (currentPage < numPages))
+   {
+      if (currentPage + 1 < numPages)
+      {
+         currentPage += 1;
+      }
+   }
+}
+
+void Book::jumpPages(int addPages)
+{
+   if (!(addPages < 0))
+   {
+      if ((bookStatus != 3) && (currentPage < numPages))
+      {
+         if (currentPage + addPages <= numPages)
+         {
+            currentPage += addPages;
+         }
+      }
+   }
+}
+
+void Book::backup(int subPages)
+{
+   if (!(subPages < 0))
+   {
+      if ((bookType != 3) && (currentPage != 1))
+      {
+         if (currentPage - subPages > 0)
+         {
+            currentPage -= subPages;
+         }
+      }
+   }
+}
+
+void Book::returnBook()
+{
+    if (bookType == 2)
+    {
+         bookType = 3;
+         bookStatus = 'n';
+         price = 0.0;
+    }
+}
